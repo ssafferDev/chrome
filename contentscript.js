b1e0c7b763d3ee9cce10a16febaeb61abd8d23df
csw3055@gmail.com <csw3055@gmail.com>
2023-01-18 15:55:05 +0900

    [FIX] css 일부 추가

--- contentscript.js
+++ contentscript.js
@@ -2,6 +2,8 @@
 let mentorBoardBtn
 let come
 let out
+let outBtn
+main()
 function initView() {
     mentorBoardBtn = document.querySelector("#header" +
         " > div.header-inner.tac.with-job.cols5 > div.column.tac.hidden-field > ul " +
@@ -36,13 +38,14 @@ function initListener() {
         let a = confirm("진짜....???")
         if (a) {
             out.style.display = "block"
+            setInBtn()
         } else {
 
         }
     });
 }
 function setOutBtn() {
-    const outBtn = document.createElement('button')
+    outBtn = document.createElement('button')
     outBtn.type = "button"
     outBtn.id = "hiddenBtn"
     outBtn.value = "test"
@@ -56,9 +59,29 @@ function setOutBtn() {
     outBtn.style.position = "relative"
     outBtn.style.verticalAlign = "middle"
     out.parentElement.append(outBtn)
+    setInBtn()
+}
+
+function setInBtn() {
+    come.style.webkitTextSizeAdjust= "none";
+    come.style.fontWeight= "300";
+    come.style.letterSpacing= "-.03em";
+    come.style.color= "#24282b";
+    come.style.lineHeight= "1.4";
+    come.style.listStyle= "none";
+    come.style.fontSize= "0";
+    come.style.boxSizing= "border-box";
+    come.style.display= "inline-block";
+    come.style.height= "78px";
+    come.style.marginLeft= "10px";
+    come.style.textAlign= "center";
+    come.style.verticalAlign= "top";
+    come.style.webkitTransition= "all .2s ease-in-out";
+    come.style.width= "78px";
+    come.style.backgroundColor= "#8c99a8";
 }
 function main() {
     initView()
-    initListener()
     setOutBtn()
+    initListener()
 }
