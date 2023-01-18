51c595d6024911263809748fe1dcd250bc7ab120
csw3055@gmail.com <csw3055@gmail.com>
2023-01-18 14:55:06 +0900

    [FIX]코드 함수화

--- contentscript.js
+++ contentscript.js
@@ -1,54 +1,64 @@
-let mentorBoardBtn = document.querySelector("#header" +
-    " > div.header-inner.tac.with-job.cols5 > div.column.tac.hidden-field > ul " +
-    " > li:nth-child(5) > a");
-const mentorBoardBtnHref = mentorBoardBtn.href
-mentorBoardBtn.input = "disabled"
 
-mentorBoardBtn.removeAttribute('href');
+let mentorBoardBtn
+let come
+let out
+function initView() {
+    mentorBoardBtn = document.querySelector("#header" +
+        " > div.header-inner.tac.with-job.cols5 > div.column.tac.hidden-field > ul " +
+        " > li:nth-child(5) > a");
 
+    mentorBoardBtn.input = "disabled"
 
-const come = document.querySelector("#wrap > div.container.main-container" +
-    " > div.content > section.main-page.sec1" +
-    " > div > div.col-md.n1 > div > div.btns > div.state.inRoomEnd")
-
-const out = document.querySelector("#wrap > div.container.main-container >" +
-    " div.content > section.main-page.sec1 > div > div.col-md.n1 > div > div.btns >" +
-    " div.state2")
-out.style.display = "none"
+    mentorBoardBtn.removeAttribute('href');
 
 
-const outBtn = document.createElement('button')
-outBtn.type = "button"
-outBtn.id = "hiddenBtn"
-outBtn.value = "test"
-outBtn.textContent = "중도퇴실"
-outBtn.style.display = "block"
-outBtn.style.fontSize= "14px"
-outBtn.style.fontWeight= "600"
-outBtn.style.height= "100%"
-outBtn.style.lineHeight= "1.5"
-outBtn.style.paddingTop= "12px"
-outBtn.style.position= "relative"
-outBtn.style.verticalAlign= "middle"
-out.parentElement.append(outBtn)
+    come = document.querySelector("#wrap > div.container.main-container" +
+        " > div.content > section.main-page.sec1" +
+        " > div > div.col-md.n1 > div > div.btns > div.state.inRoomEnd")
 
-mentorBoardBtn.addEventListener("click", function () {
-    let a = confirm("진짜....???")
-    if (a) {
-        this.href = mentorBoardBtnHref
-    }
-    else {
+    out = document.querySelector("#wrap > div.container.main-container >" +
+        " div.content > section.main-page.sec1 > div > div.col-md.n1 > div > div.btns >" +
+        " div.state2")
+    out.style.display = "none"
+}
 
-    }
-});
+function initListener() {
+    mentorBoardBtn.addEventListener("click", function () {
+        let a = confirm("진짜....???")
+        if (a) {
 
-outBtn.addEventListener("click", function () {
-    let a = confirm("진짜....???")
-    if (a) {
-        out.style.display = "block"
-    }
-    else {
+        } else {
 
-    }
-});
-//mentorBoardBtn.style.visibility = "hidden"
+        }
+    });
+
+    outBtn.addEventListener("click", function () {
+        let a = confirm("진짜....???")
+        if (a) {
+            out.style.display = "block"
+        } else {
+
+        }
+    });
+}
+function setOutBtn() {
+    const outBtn = document.createElement('button')
+    outBtn.type = "button"
+    outBtn.id = "hiddenBtn"
+    outBtn.value = "test"
+    outBtn.textContent = "중도퇴실"
+    outBtn.style.display = "block"
+    outBtn.style.fontSize = "14px"
+    outBtn.style.fontWeight = "600"
+    outBtn.style.height = "100%"
+    outBtn.style.lineHeight = "1.5"
+    outBtn.style.paddingTop = "12px"
+    outBtn.style.position = "relative"
+    outBtn.style.verticalAlign = "middle"
+    out.parentElement.append(outBtn)
+}
+function main() {
+    initView()
+    initListener()
+    setOutBtn()
+}
