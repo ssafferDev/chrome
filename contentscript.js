19ce6ae8b6edc57ef3d1292c20970ee92bc5a951
csw3055@gmail.com <csw3055@gmail.com>
2023-01-18 14:47:41 +0900

    [FEAT]간단한 퇴실버튼 숨기기'

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,54 @@
-let click = document.querySelector("#header" +
+let mentorBoardBtn = document.querySelector("#header" +
     " > div.header-inner.tac.with-job.cols5 > div.column.tac.hidden-field > ul " +
     " > li:nth-child(5) > a");
+const mentorBoardBtnHref = mentorBoardBtn.href
+mentorBoardBtn.input = "disabled"
 
-click.addEventListener("click", function () {
-    alert("hel");
-    click.href = clickHref;
+mentorBoardBtn.removeAttribute('href');
+
+
+const come = document.querySelector("#wrap > div.container.main-container" +
+    " > div.content > section.main-page.sec1" +
+    " > div > div.col-md.n1 > div > div.btns > div.state.inRoomEnd")
+
+const out = document.querySelector("#wrap > div.container.main-container >" +
+    " div.content > section.main-page.sec1 > div > div.col-md.n1 > div > div.btns >" +
+    " div.state2")
+out.style.display = "none"
+
+
+const outBtn = document.createElement('button')
+outBtn.type = "button"
+outBtn.id = "hiddenBtn"
+outBtn.value = "test"
+outBtn.textContent = "중도퇴실"
+outBtn.style.display = "block"
+outBtn.style.fontSize= "14px"
+outBtn.style.fontWeight= "600"
+outBtn.style.height= "100%"
+outBtn.style.lineHeight= "1.5"
+outBtn.style.paddingTop= "12px"
+outBtn.style.position= "relative"
+outBtn.style.verticalAlign= "middle"
+out.parentElement.append(outBtn)
+
+mentorBoardBtn.addEventListener("click", function () {
+    let a = confirm("진짜....???")
+    if (a) {
+        this.href = mentorBoardBtnHref
+    }
+    else {
+
+    }
 });
-const clickHref = click.href
-//click.removeAttribute('href');
+
+outBtn.addEventListener("click", function () {
+    let a = confirm("진짜....???")
+    if (a) {
+        out.style.display = "block"
+    }
+    else {
+
+    }
+});
+//mentorBoardBtn.style.visibility = "hidden"
